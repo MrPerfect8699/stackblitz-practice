ff7dc794800d1589b6c4b912995876417258c705
MrPerfect8699 <sshahapurkar0@gmail.com>
2020-02-18 11:26:36 +0530

    table with sort, 3 files modified, 3 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import {OrderModule } from 'ngx-order-pipe';
+ 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { Page1Component } from './page1/page1.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, OrderModule ],
+  declarations: [ AppComponent, Page1Component ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
